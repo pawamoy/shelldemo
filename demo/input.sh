ebf3255a21c1606ac8dc8ede76a5c1d02d2707ac
Timothée Mazzucotelli <timothee.mazzucotelli@pm.me>
2019-05-07 11:58:57 +0200

    docs: Add demo

---
+++ demo/input.sh
@@ -0,0 +1,15 @@
+# Hello Demo!  # demo: sleep 1
+# I'm shelldemo's demo, done with shelldemo itself!  # demo: sleep 1
+# Let's try to sleep a bit!  # demo: sleep 1
+sleep 2  # demo: sleep 2
+# Nice!  # demo: sleep 1
+# Let's increase the typing speed!  # demo: sleep 1; charinterval=0.02
+# mmmmmmmmmmmmmmMMmmMmMMmmmmmmmmmmmmmmmmmmm  mzdkzmzk
+# GOT TO GO FAST! GOT TO GO FAST!
+# GOT TO GO FAST! GOT TO GO FAST!
+# GOT TO GO FAST! GOT TO GO FAST!
+# GOT TO GO FAST! GOT TO GO FAST!
+# GOT TO GO FAST! GOT TO GO FAST!
+# GOT TO GO FAST! GOT TO GO FAST!  # demo: charinterval=0.4
+# That was too fast. Now it's too slow! # demo: sleep 1; charinterval=0.05
+# Your turn now :D!  # demo: sleep 2
